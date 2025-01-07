cd5236928746962dcb25c199bf7d05f32eab22c2
Zenocloud-Kelly <zenocloudkelly@gmail.com>
2025-01-07 15:50:20 +0800

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a6bd3f1de158bb61090e0c8053df93a10cb548
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "434391066"
+    serviceWorkerVersion: "3490776273"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -13,11 +13,11 @@ const RESOURCES = {"canvaskit/skwasm.wasm": "9f0c0c02b82a910d12ce0543ec130e60",
 "canvaskit/chromium/canvaskit.wasm": "b1ac05b29c127d86df4bcfbf50dd902a",
 "canvaskit/chromium/canvaskit.js": "671c6b4f8fcc199dcc551c7bb125f239",
 "canvaskit/canvaskit.js": "66177750aff65a66cb07bb44b8c6422b",
-"index.html": "23b1f7dad472af143e234cd78f02950d",
-"/": "23b1f7dad472af143e234cd78f02950d",
+"index.html": "7b9bb393535d90c62760c49db7637f86",
+"/": "7b9bb393535d90c62760c49db7637f86",
 "hls_player.html": "356f5b299fea196656bd71a45868d247",
 "version.json": "27facc3d327d18dc6f00e3ebc8828f11",
-"main.dart.js": "377543c2074c4078b0f74395eacc4647",
+"main.dart.js": "02a48a69ec127a14a0d90cc363313ac3",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
 "assets/fonts/MaterialIcons-Regular.otf": "0db35ae7a415370b89e807027510caf0",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
@@ -27,7 +27,7 @@ const RESOURCES = {"canvaskit/skwasm.wasm": "9f0c0c02b82a910d12ce0543ec130e60",
 "assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "flv_player.html": "2d58e48b3e3bf20ac48a51e924cf88b6",
-"flutter_bootstrap.js": "42b393945b56af5714f9c5273fd0c430"};
+"flutter_bootstrap.js": "935b8d9daa1c876ffe2a9c2a0157f1b9"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
